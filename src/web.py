78dba3664937a8d8e3d28dfb1042a839451e6167
Richard Hájek <richard.m.hajek@gmail.com>
2020-07-16 21:36:34 +0200

    Added REST interfae

---
+++ src/web.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from collections import defaultdict
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
+
+PORT_NUMBER = 8080
+
+from src import load
+from src.load import *
+
+# This class will handle any incoming request from
+# a browser
+class myHandler(BaseHTTPRequestHandler):
+
+    # Handler for the GET requests
+    def do_GET(self):
+        print('Processing request with question... : ', end="")
+
+        if self.path.__len__() < 3:
+            self.path = "/?"
+
+        question = urllib.parse.parse_qs(self.path[2:])["question"][0]
+
+        print(question)
+
+
+        model, tokenizer = m.get_model()
+        status, answer = load.ask_question(question, model, tokenizer)
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        # Send the html message
+
+        self.wfile.write(str({"status": str(status), "answer": answer}).encode())
+
+        #self.wfile.write(str(self.headers).encode())
+        #self.wfile.write(b"\n\n\n")
+        #self.wfile.write(str(self.path).encode())
+        return
+
+
+try:
+    # Create a web server and define the handler to manage the
+    # incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print('Started httpserver on port ', PORT_NUMBER)
+
+    # Wait forever for incoming http requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print('^C received, shutting down the web server')
+    server.socket.close()
